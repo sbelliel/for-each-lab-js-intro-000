1239f1627d50d7b95e0e1e28d88312c8f66106a0
Sam Belliel <sbelliel@gmail.com>
2018-08-17 00:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function iterativeLog(array) {
 
 function iterate(callback) {
 var animals = ["dog", "cat", "rhino"];
-animals.forEach(function(callback) {
+animals.forEach(function(element) {
 console.log(`${callback}`)
 })
 }
